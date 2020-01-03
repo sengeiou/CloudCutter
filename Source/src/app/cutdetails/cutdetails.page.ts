607b6cd6984744aae834aec0363971b433783fc4
alucard263096 <alucard263096@126.com>
2020-01-03 18:32:36 +0800

    commit

--- Source/src/app/cutdetails/cutdetails.page.ts
+++ Source/src/app/cutdetails/cutdetails.page.ts
@@ -172,19 +172,19 @@ export class CutdetailsPage extends AppBase {
                                   this.ngzone.run(() => { });
                                 }
                               });
-                            }, 2000);
+                            }, 500);
                           } else {
                             this.cuterror = "设置刀压出错：" + ret3;
                             this.ngzone.run(() => { });
                           }
                         });
-                      }, 2000);
+                      }, 500);
                     } else {
                       this.cuterror = "设置刀速出错：" + ret2;
                       this.ngzone.run(() => { });
                     }
                   });
-                }, 2000);
+                }, 500);
               } else {
                 this.cuterror = device.machinestatus_name;
                 this.ngzone.run(() => { });
