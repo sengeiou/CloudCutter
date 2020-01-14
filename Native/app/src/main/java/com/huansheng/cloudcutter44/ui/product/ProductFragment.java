81022569fffdea76b568da56b0ff711938c34d9d
alucard263096 <alucard263096@126.com>
2020-01-14 17:03:47 +0800

    Commit for new java version

---
+++ Native/app/src/main/java/com/huansheng/cloudcutter44/ui/product/ProductFragment.java
@@ -0,0 +1,38 @@
+package com.huansheng.cloudcutter44.ui.product;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.huansheng.cloudcutter44.R;
+
+public class ProductFragment extends Fragment {
+
+    private ProductViewModel mViewModel;
+
+    public static ProductFragment newInstance() {
+        return new ProductFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.product_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ProductViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
