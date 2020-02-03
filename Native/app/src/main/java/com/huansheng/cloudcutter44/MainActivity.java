e848a8892887bdbc5613ae899d36185fb8226691
alucard263096 <alucard263096@126.com>
2020-02-03 17:15:38 +0800

    添加表控件

--- Native/app/src/main/java/com/huansheng/cloudcutter44/MainActivity.java
+++ Native/app/src/main/java/com/huansheng/cloudcutter44/MainActivity.java
@@ -104,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_product, R.id.navigation_mine)
+                R.id.navigation_home, R.id.navigation_product,R.id.navigation_chart, R.id.navigation_mine)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
--- Native/app/src/main/java/com/huansheng/cloudcutter44/ui/chart/ChartFragment.java
+++ Native/app/src/main/java/com/huansheng/cloudcutter44/ui/chart/ChartFragment.java
@@ -8,14 +8,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 import com.huansheng.cloudcutter44.R;
+import com.huansheng.cloudcutter44.ui.home.HomeFragment;
 
 public class ChartFragment extends Fragment {
-
+    private static int ShowType=0;
+    private TabLayout tabhot;
+    private TabItem t0;
+    private TabItem t1;
+    private View daily;
+    private View models;
     private ChartViewModel mViewModel;
 
     public static ChartFragment newInstance() {
@@ -25,7 +34,42 @@ public class ChartFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.chart_fragment, container, false);
+        View root= inflater.inflate(R.layout.chart_fragment, container, false);
+
+        this.tabhot = root.findViewById(R.id.tabhot);
+        this.daily = root.findViewById(R.id.daily);
+        this.models = root.findViewById(R.id.models);
+        this.t0 = root.findViewById(R.id.t0);
+        this.t1 = root.findViewById(R.id.t1);
+
+        this.tabhot.getTabAt(ChartFragment.ShowType).select();
+
+        this.tabhot.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+//                Toast toast=Toast.makeText(getContext(),"Toast提示消息"+tab.getPosition(),Toast.LENGTH_SHORT    );
+//                toast.show();
+                ChartFragment.ShowType=tab.getPosition();
+                setTabVisable();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+
+
+        return root;
     }
 
     @Override
@@ -35,4 +79,10 @@ public class ChartFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    public void setTabVisable() {
+        int position=ChartFragment.ShowType;
+        Log.e("setTabVisable", String.valueOf(position));
+        this.daily.setVisibility(position == 0 ? View.VISIBLE : View.GONE);
+        this.models.setVisibility(position == 1 ? View.VISIBLE : View.GONE);
+    }
 }
