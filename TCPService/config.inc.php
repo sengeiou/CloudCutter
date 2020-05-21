28ec08ad900f76d9cb25fdafe9bfb237a4146ef0
蔡小司 <alucard263096@126.com>
2020-05-21 18:55:07 +0800

    Update config.inc.php

--- TCPService/config.inc.php
+++ TCPService/config.inc.php
@@ -17,10 +17,10 @@ $CONFIG['SERVERIP']='120.77.151.197';
 
 #[Database]
 $CONFIG['database']['provider']	= 'mysql';  //mssql,sqlsrv
-$CONFIG['database']['host']		= 'mysql.app-link.org';  
-$CONFIG['database']['database']	= 'alucard263096_cloudcutter';  
-$CONFIG['database']['user']		= 'alucard263096';  
-$CONFIG['database']['psw']		= 'a6c3bc0575df7d9d676890e861130a9a'; 
+$CONFIG['database']['host']		= '';  
+$CONFIG['database']['database']	= '';  
+$CONFIG['database']['user']		= '';  
+$CONFIG['database']['psw']		= ''; 
 
 
 #[File upload]
@@ -29,8 +29,8 @@ $CONFIG['fileupload']['oss']	= true;//改为oss上传的话
 if($CONFIG['fileupload']['oss']==true){
 	$CONFIG['fileupload']['upload_path']="aliyun";//改为qiniu则用七牛的云
 	$CONFIG['fileupload']['upload_path']	= "http://applinkupload.oss-cn-shenzhen.aliyuncs.com";
-	$CONFIG['fileupload']['accesskeyid']    = "LTAIkBoCGZOPEXFh";
-	$CONFIG['fileupload']['accesskeysecret']    = "bN7Gnq1q6l6TDlVK8yQDcjcCzY8sQt";
+	$CONFIG['fileupload']['accesskeyid']    = "";
+	$CONFIG['fileupload']['accesskeysecret']    = "";
 	$CONFIG['fileupload']['endpoint']    = "oss-cn-shenzhen-internal.aliyuncs.com";
 	$CONFIG['fileupload']['bucket']    = "applinkupload";
 	$CONFIG['fileupload']['bucket_path']    = "";
