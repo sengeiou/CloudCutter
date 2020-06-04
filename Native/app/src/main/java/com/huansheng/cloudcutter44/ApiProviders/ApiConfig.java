af621a2d79eb86a6d68a39d21b2581b237c6c8d0
Tyrant24 <tigerhi@qq.com>
2020-06-04 19:26:21 +0800

    0604

--- Native/app/src/main/java/com/huansheng/cloudcutter44/ApiProviders/ApiConfig.java
+++ Native/app/src/main/java/com/huansheng/cloudcutter44/ApiProviders/ApiConfig.java
@@ -23,22 +23,45 @@ package com.huansheng.cloudcutter44.ApiProviders;
 //    }
 //}
 
+//public class ApiConfig {
+//    public  static String encryptionkey="abcd1234";
+//    public static String getApiUrl() {
+//        return "https://cmsdev.app-link.org/alucard263096/cloudcutter/api/";
+//    }
+//    public static String getUploadPath() {
+//        return "https://alioss.app-link.org/alucard263096/cloudcutter/";
+//    }
+//    public static String getFileUploadAPI() {
+//        return "https://cmsdev.app-link.org/alucard263096/cloudcutter/fileupload";
+//    }
+//
+//    public static String getLogUrl() {
+//        return "https://cmsdev.app-link.org/users/alucard263096/cloudcutter/logs/";
+//    }
+//
+//    public static String photoStyle(){
+//        return "?x-oss-process=image/resize,p_50/quality,q_100";
+//    }
+//    public static String photoStyle2(String size){
+//        return "?x-oss-process=image/resize,p_"+size+"/quality,q_100";
+//    }
+//}
+
+
 public class ApiConfig {
     public  static String encryptionkey="abcd1234";
     public static String getApiUrl() {
-        return "https://cmsdev.app-link.org/alucard263096/cloudcutter/api/";
+        return "https://app.mobile-films.com/api/";
     }
     public static String getUploadPath() {
-        return "https://alioss.app-link.org/alucard263096/cloudcutter/";
+        return "https://mobile-films.oss-cn-hongkong.aliyuncs.com/";
     }
     public static String getFileUploadAPI() {
-        return "https://cmsdev.app-link.org/alucard263096/cloudcutter/fileupload";
+        return "https://app.mobile-films.com/fileupload";
     }
-
     public static String getLogUrl() {
-        return "https://cmsdev.app-link.org/users/alucard263096/cloudcutter/logs/";
+        return "https://app.mobile-films.com/users/logs/";
     }
-
     public static String photoStyle(){
         return "?x-oss-process=image/resize,p_50/quality,q_100";
     }
